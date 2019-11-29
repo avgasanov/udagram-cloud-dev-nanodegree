6c7813eb1f4ca683e1e0c18fa8e0d4ef291cda08
AVGasanov <avgasanov@gmail.com>
2019-11-29 11:28:49 +0400

    Implement stubbed "filteredimage" api

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,7 +28,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get('/filteredimage', 
+    async ( req: Request, res: Response) => {
+      const { image_url } = req.query
 
+      if (!image_url) {
+        return res.status(400).send("Please provide url param for image")
+      } 
+      return filterImageFromURL(image_url)
+      .then(img => {
+        return res.status(200).sendFile(img, 
+          () => deleteLocalFiles([img]))
+      })
+  });
+
+  
   //! END @TODO1
   
   // Root Endpoint
