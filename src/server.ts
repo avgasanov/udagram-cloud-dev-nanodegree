8b628e21704177b86b3ab19eacd8ec3a32e568ba
AVGasanov <avgasanov@gmail.com>
2019-11-29 11:34:31 +0400

    Fix datatypes for ts

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   const app = express();
 
   // Set the network port
-  const port = process.env.PORT || 8082;
+  const port: String = process.env.PORT || '8082';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
@@ -47,7 +47,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
